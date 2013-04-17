e450344a155c01488f4b1221e50d2ad2c985b721
James Miller <bensie@gmail.com>
2013-04-17 13:03:46 -0700

    Write the node.json file as well, assume we are in the chef root or pass it as argv[1]

--- write_data_bags.rb
+++ write_data_bags.rb
@@ -1,12 +1,16 @@
+require "fileutils"
 require "bundler"
 Bundler.require
 
 token = ARGV[0]
-output_path = ARGV[1] || Dir.pwd
+chef_root_dir = ARGV[1] || Dir.pwd
+FileUtils.mkdir_p "#{chef_root_dir}/data_bags/users"
 endpoint = "https://amigobooth.com/api/v1/services/devices"
 body = Http.with_headers("X-AmigoBooth-Token" => token).get endpoint
 
 devices = MultiJson.load body
+
+data_bag_users = []
 
 devices.each do |device|
   username = device["id"].gsub("-", "")
@@ -17,6 +21,15 @@ devices.each do |device|
   data_bag["home"]       = "/home/#{username}"
   data_bag["ssh_keys"]   = [device["ssh_public_key"]]
   data_bag["ssh_keygen"] = false
+  data_bag["shell"]      = "/usr/sbin/nologin"
 
-  File.open("#{output_path}/#{username}.json", "w") {|f| f.write(MultiJson.dump(data_bag, pretty: true)) }
+  File.open("#{chef_root_dir}/data_bags/users/#{username}.json", "w") {|f| f.write(MultiJson.dump(data_bag, pretty: true)) }
+  data_bag_users << username
 end
+
+node_info = {
+  run_list: ["user::data_bag"],
+  users: data_bag_users
+}
+
+File.open("#{chef_root_dir}/node.json", "w") {|f| f.write(MultiJson.dump(node_info, pretty: true)) }
